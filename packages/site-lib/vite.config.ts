a33749792bb9116e896d588204580d11c8df0cfd
gwionrob <robertsongwion21@gmail.com>
2023-08-15 00:09:25 +0100

    update index ref

--- packages/site-lib/vite.config.ts
+++ packages/site-lib/vite.config.ts
@@ -1,33 +1,32 @@
-import react from '@vitejs/plugin-react-swc';
-import path from 'node:path';
-import { defineConfig } from 'vite';
-import dts from 'vite-plugin-dts';
+import react from "@vitejs/plugin-react-swc";
+import path from "node:path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-        dts({
-            insertTypesEntry: true,
-        }),
-    ],
-    build: {
-        lib: {
-            entry: path.resolve(__dirname, 'src/index.ts'),
-            name: 'Site Library',
-            formats: ['es', 'umd'],
-            fileName: (format) => `site-lib.${format}.js`,
-        },
-        rollupOptions: {
-            external: ['react', 'react-dom', 'styled-components'],
-            output: {
-                globals: {
-                    react: 'React',
-                    'react-dom': 'ReactDOM',
-                    'styled-components': 'styled',
-                },
-            },
-        },
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "index.ts"),
+      name: "Site Library",
+      formats: ["es", "umd"],
+      fileName: (format) => `site-lib.${format}.js`,
     },
-
-})
+    rollupOptions: {
+      external: ["react", "react-dom", "styled-components"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "styled-components": "styled",
+        },
+      },
+    },
+  },
+});
